2fe818963a1e27a6d54fe6954cd600df180ed171
BobEx01 <boburi9000@mail.ru>
2025-07-16 16:04:56 +0500

    Update utils.py

--- handlers/utils.py
+++ handlers/utils.py
@@ -1,1 +1,7 @@
-# utils.py - Yordamchi funksiyalar
+# handlers/utils.py
+
+from telegram import ReplyKeyboardMarkup
+
+def back_button():
+    keyboard = [["⬅️ Orqaga"]]
+    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
