02516e8de31d8810b288d681c815156c6d8c567a
BobEx01 <boburi9000@mail.ru>
2025-07-16 23:27:38 +0500

    Update utils.py

--- handlers/utils.py
+++ handlers/utils.py
@@ -1,6 +1,5 @@
 from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import ContextTypes
-from handlers.utils import asosiy_menu
 
 # /start komandasi uchun funksiya
 async def boshlash(update: Update, context: ContextTypes.DEFAULT_TYPE):
