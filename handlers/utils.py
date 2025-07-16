39c7e0b70b8c3d120ad11b1350a383f84f59bcad
BobEx01 <boburi9000@mail.ru>
2025-07-16 23:23:18 +0500

    Update utils.py

--- handlers/utils.py
+++ handlers/utils.py
@@ -1,7 +1,17 @@
-# handlers/utils.py
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import ContextTypes
+from handlers.utils import asosiy_menu
 
-from telegram import ReplyKeyboardMarkup
-
-def back_button():
-    keyboard = [["⬅️ Orqaga"]]
-    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+# /start komandasi uchun funksiya
+async def boshlash(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "👋 Assalomu alaykum, BobEx botiga xush kelibsiz!\n\n"
+        "Quyidagi menyulardan birini tanlang:\n\n"
+        "🚛 Yuk uchun e'lon berish\n"
+        "🚚 Shofyor e'lon berish\n"
+        "📦 Yuk e'lonlarini ko‘rish\n"
+        "🚚 Shofyor e'lonlarini ko‘rish\n"
+        "💳 Hisobim — balans va hisob to‘ldirish\n"
+        "🎁 Paketlar — VIP tarif va bonus paketlar",
+        reply_markup=asosiy_menu()
+    )
