a46e3028648963a06af611a89a9be1247381008a
BobEx01 <boburi9000@mail.ru>
2025-07-20 00:01:05 +0500

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,21 @@ TOKEN = '7653469544:AAFH4xoRxu8-_nWy0CR1gXA1Nkv1txt3gqc'
 ADMIN_ID = 8080091052
 ADMIN_USERNAME = '@bobex_uz'
 
+# Telefon raqam olish narxi
 RAQAM_NARX = 28000
-PREMIUM_ELON_NARX = 10000
-VIP_NARX = 1000000
+
+# E'lon xizmati narxlari
+VIP_NARX = 45000          # VIP e’lon narxi
+SUPER_NARX = 90000        # Super e’lon narxi
+
+# Oddiy Premium narx - endi eskirgan, kerak bo‘lsa qoldiring, bo‘lmasa olib tashlang
+# PREMIUM_ELON_NARX = 10000
+
+# Telefon raqam bonuslari
+VIP_BONUS_RAQAMLAR = 1     # VIP olganlarga necha dona raqam bepul
+SUPER_BONUS_RAQAMLAR = 3   # Super olganlarga necha dona raqam bepul
+
+# Paket narxlari
 PAKET_10_NARX = 186000
+
+# E'lon muddatlari va boshqa sozlamalar ham kerak bo‘lsa shu yerga qo‘shiladi
