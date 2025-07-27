07b46520dcd1a11328a7747dfa5690f6cb06b844
BobEx01 <boburi9000@mail.ru>
2025-07-27 08:02:15 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,12 @@
-import os
-from dotenv import load_dotenv
+# config.py
 
-load_dotenv()
+BOT_TOKEN = "7653469544:AAEuDWAsJTJ404V1AFIcx_lkJNUkLi_kgmU"
+ADMIN_ID = 8080091052
+ADMIN_USERNAME = "@bobex_uz"
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
-ADMIN_USERNAME = os.getenv("ADMIN_USERNAME")
-
-RAQAM_NARX = int(os.getenv("RAQAM_NARX"))
-VIP_NARX = int(os.getenv("VIP_NARX"))
-SUPER_NARX = int(os.getenv("SUPER_NARX"))
-PAKET_10_NARX = int(os.getenv("PAKET_10_NARX"))
-VIP_ELON_NARX = int(os.getenv("VIP_ELON_NARX"))
-SUPER_ELON_NARX = int(os.getenv("SUPER_ELON_NARX"))
+RAQAM_NARX = 28000
+VIP_NARX = 45000
+SUPER_NARX = 90000
+PAKET_10_NARX = 186000
+VIP_ELON_NARX = 45000
+SUPER_ELON_NARX = 90000
