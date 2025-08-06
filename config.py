1d5cb7e119721f78e0332695e6d6fed056506275
BobEx01 <boburi9000@mail.ru>
2025-08-06 09:54:51 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,17 @@
 # config.py
 import os
 
+# Bot tokeni (avval ENV'dan oladi, bo'lmasa hozirgi tokenni ishlatadi)
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7653469544:AAEuDWAsJTJ404V1AFIcx_lkJNUkLi_kgmU")
+
+# Admin ma'lumotlari
 ADMIN_ID = int(os.getenv("ADMIN_ID", "8080091052"))
 ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "@bobex_uz")
 
-RAQAM_NARX = 28000
-VIP_NARX = 1000000
-SUPER_NARX = 90000
-PAKET_10_NARX = 186000
-VIP_ELON_NARX = 45000
-SUPER_ELON_NARX = 90000
+# Narx sozlamalari
+RAQAM_NARX = int(os.getenv("RAQAM_NARX", "28000"))
+VIP_NARX = int(os.getenv("VIP_NARX", "1000000"))
+SUPER_NARX = int(os.getenv("SUPER_NARX", "90000"))
+PAKET_10_NARX = int(os.getenv("PAKET_10_NARX", "186000"))
+VIP_ELON_NARX = int(os.getenv("VIP_ELON_NARX", "45000"))
+SUPER_ELON_NARX = int(os.getenv("SUPER_ELON_NARX", "90000"))
